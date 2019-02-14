e570570fbb672382cb4ff1e1335a7e52971d57ce
cbaeza <cbaeza@gk-software.com>
2019-02-14 23:57:20 +0100

    extract Data Base service

---
+++ src/main/java/com/cbaeza/service/DataBaseService.java
@@ -0,0 +1,66 @@
+package com.cbaeza.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.jooq.Condition;
+import org.jooq.DSLContext;
+import org.jooq.Field;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.Table;
+import org.jooq.impl.DSL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * Data Base service responsible to encapsulate DB calls
+ */
+@Service
+public class DataBaseService {
+
+  private static final Logger LOG = LoggerFactory.getLogger(DataBaseService.class.getName());
+
+  @Value("${db.url}")
+  private String url;
+
+  @Value("${db.user}")
+  private String user;
+
+  @Value("${db.password}")
+  private String password;
+
+  public Result<Record> executeSelectQuery(final Table table) {
+    return executeSelectQuery(table, null);
+  }
+
+  public Result<Record> executeSelectQuery(final Table table, final Condition condition) {
+    try (Connection conn = DriverManager.getConnection(url, user, password)) {
+      DSLContext create = DSL.using(conn, SQLDialect.H2);
+      if (condition != null) {
+        return create.select().from(table).where(condition).fetch();
+      } else {
+        return create.select().from(table).fetch();
+      }
+    } catch (Exception e) {
+      LOG.error("executeSelectQuery", e);
+    }
+    return null;
+  }
+
+  @SuppressWarnings("unchecked")
+  public int executeUpdateQuery(final Table table, final Condition condition, final Field row, final String value) {
+    try (Connection conn = DriverManager.getConnection(url, user, password)) {
+      DSLContext create = DSL.using(conn, SQLDialect.H2);
+      if (condition != null) {
+        return create.update(table).set(row, value).where(condition).execute();
+      }
+    } catch (Exception e) {
+      LOG.error("executeSelectQuery", e);
+    }
+    return -1;
+  }
+}
--- src/main/java/com/cbaeza/service/PersonService.java
+++ src/main/java/com/cbaeza/service/PersonService.java
@@ -1,20 +1,11 @@
 package com.cbaeza.service;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.List;
 
-import org.jooq.Condition;
-import org.jooq.DSLContext;
-import org.jooq.Field;
 import org.jooq.Record;
 import org.jooq.Result;
-import org.jooq.SQLDialect;
-import org.jooq.Table;
-import org.jooq.impl.DSL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.cbaeza.dto.PersonDto;
@@ -27,31 +18,28 @@ import com.cbaeza.util.MapUtils;
  */
 @Service
 public class PersonService {
-
   private static final Logger LOG = LoggerFactory.getLogger(PersonService.class.getName());
 
-  @Value("${db.url}")
-  private String url;
+  private final DataBaseService dataBaseService;
 
-  @Value("${db.user}")
-  private String user;
-
-  @Value("${db.password}")
-  private String password;
+  public PersonService(DataBaseService dataBaseService) {
+    this.dataBaseService = dataBaseService;
+  }
 
   public List<PersonDto> getAllPersons() {
-    Result<Record> result = executeSelectQuery(Person.PERSON);
+    Result<Record> result = dataBaseService.executeSelectQuery(Person.PERSON);
     return MapUtils.mapPersons(result);
   }
 
   public PersonDto getPerson(int id) {
-    Result<Record> result = executeSelectQuery(Person.PERSON, Person.PERSON.ID.eq(id));
+    Result<Record> result = dataBaseService.executeSelectQuery(Person.PERSON, Person.PERSON.ID.eq(id));
     return MapUtils.mapPerson(result);
   }
 
   public ResponseDto updatePerson(final int id, final PersonDto personDto) {
     if (personDto != null) {
-      int i = executeUpdateQuery(Person.PERSON, Person.PERSON.ID.eq(id), Person.PERSON.NAME, personDto.getName());
+      int i = dataBaseService.executeUpdateQuery(Person.PERSON, Person.PERSON.ID.eq(id), Person.PERSON.NAME,
+          personDto.getName());
       LOG.info(">>>> result update: " + i);
       if (i == -1) {
         return new ResponseDto(i + " NO row affected");
@@ -60,36 +48,6 @@ public class PersonService {
       }
     }
     return new ResponseDto("personDto can not be null");
-  }
-
-  private Result<Record> executeSelectQuery(Table table) {
-    return executeSelectQuery(table, null);
-  }
-
-  private Result<Record> executeSelectQuery(Table table, Condition condition) {
-    try (Connection conn = DriverManager.getConnection(url, user, password)) {
-      DSLContext create = DSL.using(conn, SQLDialect.H2);
-      if (condition != null) {
-        return create.select().from(table).where(condition).fetch();
-      } else {
-        return create.select().from(table).fetch();
-      }
-    } catch (Exception e) {
-      LOG.error("executeSelectQuery", e);
-    }
-    return null;
-  }
-
-  private int executeUpdateQuery(Table table, Condition condition, Field row, String value) {
-    try (Connection conn = DriverManager.getConnection(url, user, password)) {
-      DSLContext create = DSL.using(conn, SQLDialect.H2);
-      if (condition != null) {
-        return create.update(table).set(row, value).where(condition).execute();
-      }
-    } catch (Exception e) {
-      LOG.error("executeSelectQuery", e);
-    }
-    return -1;
   }
 
 }
